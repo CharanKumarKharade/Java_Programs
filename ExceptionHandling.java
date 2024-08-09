f9d52cfa50a1565b5701c9bf2a26d1b2a0d45a63
unknown <CharanKumarKharadeS>
2024-08-09 18:58:49 +0530

    added new program

--- ExceptionHandling.java
+++ ExceptionHandling.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
+import java.util.*;
 public class ExceptionHandling {
+    public static void main(String [] args ){
+        System.out.println("Enter an number");
+        Scanner sc= new Scanner(System.in);
+        int number=sc.nextInt(),i;
+        try{
+            for(i=10;i>=0;i-=2){
+                System.out.println(number/i);
+            }
+        }
+        catch(ArithmeticException error){
+            System.out.println("Error:"+error.getMessage());
+        }
+        finally{
+            System.out.println("Ended the program by skipping the divisibility by 0");
+        }
+    }
 }
--- out/production/Projects/README.md
+++ out/production/Projects/README.md
@@ -2,6 +2,9 @@
 Used to practice all Patterns, DSA for easier and better understanding
 A quick reference towards Java and Sample programs
 
----Program 1: Factorial of an number---
----Program 2: Odd or Even number---
+---Program 1: Factorial of an number---</br>
+---Program 2: Odd or Even number---</br>
+---Program 3: Palindrome number---</br>
+---Program 4: Reverse an number---</br>
+---Program 5: Buzz number---</br>
 ----------------------------------------------------
